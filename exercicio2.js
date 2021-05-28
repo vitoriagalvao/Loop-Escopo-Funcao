161df14e44f49cf302244cc291e2304d9ce38839
Vitória Galvão <vitoriaggalvao19@gmail.com>
2021-05-27 23:20:04 -0300

    Adicionando resolução2

---
+++ exercicio2.js
@@ -0,0 +1,9 @@
+//2 - crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function diasEmHoras(dias) {
+  let hora = 24;
+  const resultado = dias * hora;
+  return resultado;
+}
+
+console.log(diasEmHoras(3));
